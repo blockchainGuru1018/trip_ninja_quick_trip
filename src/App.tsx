458655b5b68a5172f1780c51960a5fd00ebf2a48
Rob <rob.dumont99@gmail.com>
2020-05-21 11:11:38 -0400

    add logo, icon, and import bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <div className="container">
+        <div className="row">
+            <div className="col-lg">
+              <h1>Welcome to QuickTrip</h1>
+            </div>
+        </div>      
+      </div>
     </div>
+    
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 
 ReactDOM.render(

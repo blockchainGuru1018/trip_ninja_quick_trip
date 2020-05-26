f1d4d21a26cd9615a3468035cf2341c3767347fe
Rob <rob.dumont99@gmail.com>
2020-05-26 14:10:23 -0400

    nav styling

--- src/common/NavBar.tsx
+++ src/common/NavBar.tsx
@@ -14,7 +14,7 @@ class NavBar extends React.Component {
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
-            <li className="nav-item active">
+            <li className="nav-item">
               <a className="nav-link" href="/search/">Flight Search<span className="sr-only">(current)</span></a>
             </li>
           </ul>
--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import './index.css';
 import App from './App';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
-import './index.css';
 
 
 ReactDOM.render(

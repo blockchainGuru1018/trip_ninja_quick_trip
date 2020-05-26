a61bd8a0e79c960fa246c60b00963518eeee1607
Rob <rob.dumont99@gmail.com>
2020-05-26 10:51:20 -0400

    Update NavBar.tsx

--- src/common/NavBar.tsx
+++ src/common/NavBar.tsx
@@ -15,7 +15,7 @@ class NavBar extends React.Component {
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav">
               <li className="nav-item active">
-                <a className="nav-link" href="/">Flight Search<span className="sr-only">(current)</span></a>
+                <a className="nav-link" href="/search/">Flight Search<span className="sr-only">(current)</span></a>
               </li>
             </ul>
           </div>

5b2ddf0bace786376fe2536eda87a8f74e646269
k.mcclymont <kieran.mcclymont@tripninja.io>
2020-05-26 14:13:54 -0300

    - initial commit for react testing setup

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,15 +1,8 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
 import App from './App';
+import ReactDOM from 'react-dom';
 
-test('renders learn react link', () => {
-  const { getByText } = render(
-    <Provider store={store}>
-      <App />
-    </Provider>
-  );
-
-  expect(getByText(/learn/i)).toBeInTheDocument();
+it('renders app', () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<App />, div);
 });

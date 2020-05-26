6f767e5940e951a203207f28b8cdb320201352af
Rob <rob.dumont99@gmail.com>
2020-05-26 09:20:58 -0400

    Add in navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import NavBar from './common/NavBar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <div className="container">
         <div className="row">
             <div className="col-lg">

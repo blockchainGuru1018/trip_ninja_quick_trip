12c6cfb5a69ff1c706303effaa0cf85cc30698b7
k.mcclymont <kieran.mcclymont@tripninja.io>
2020-05-28 08:09:28 -0300

    - initial commit for api setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,28 @@
 import React from 'react';
 import './App.css';
+import API from './api'
 
-function App() {
-  return (
-    <div className="App">
-      <div className="container">
-        <div className="row">
-          <div className="col-lg">
-            <h1>Welcome to QuickTrip</h1>
-            <h2>Something else</h2>
+
+class App extends React.Component {
+  componentDidMount() {
+    console.log('the component mounted')
+    API.get('get_all_users/')
+      .then((response: JSON) => console.log(response))
+  }
+  render() {
+    return (
+      <div className="App">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg">
+              <h1>Welcome to QuickTrip</h1>
+              <h2>Something else</h2>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;

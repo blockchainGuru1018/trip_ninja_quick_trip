ecc9e11bfaeb304c575c9b0c766651a231886f3e
Rob <rob.dumont99@gmail.com>
2020-05-28 14:15:38 -0400

    positioning

--- src/trip/search/TripPath.tsx
+++ src/trip/search/TripPath.tsx
@@ -4,10 +4,12 @@ import FlightIcon from '@material-ui/icons/Flight';
 class TripPath extends React.Component {
   render() {
     return (
-      <div className="float-right">
-        <span>YHZ</span>
-        <FlightIcon className="rotate-90"/>
-        <span>YOW</span>
+      <div className="trip-path float-right">
+        <p>
+          <span>YHZ </span>
+          <FlightIcon className="rotate-90"/>
+          <span> YOW</span>
+        </p>
       </div>
     )
   }

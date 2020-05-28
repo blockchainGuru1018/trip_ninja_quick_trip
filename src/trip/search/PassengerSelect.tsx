d94f0923fb01f5aac01047893a188336dbf29508
Rob <rob.dumont99@gmail.com>
2020-05-28 10:41:38 -0400

    passenger base

--- src/trip/search/PassengerSelect.tsx
+++ src/trip/search/PassengerSelect.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import FormControl from '@material-ui/core/FormControl';
+import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
+import PersonAddOutlinedIcon from '@material-ui/icons/PersonAddOutlined';
 
 class PassengerSelect extends React.Component {
   render() {
     return (
       <div>
-        <p>Passengers</p>
+        <FormControl fullWidth>
+          <Input
+            id="input-with-icon-adornment"
+            placeholder="Passengers"
+            startAdornment={
+              <InputAdornment position="start">
+                <PersonAddOutlinedIcon />
+              </InputAdornment>
+            }
+          />
+        </FormControl>
       </div>
     )
   }

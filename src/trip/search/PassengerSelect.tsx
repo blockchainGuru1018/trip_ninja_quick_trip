d8afd4c4b4ecfe25d8e214cf1580a5bb95d51db2
Rob <rob.dumont99@gmail.com>
2020-05-28 21:04:40 -0400

    Update PassengerSelect.tsx

--- src/trip/search/PassengerSelect.tsx
+++ src/trip/search/PassengerSelect.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import PassengerList from '../../assets/data/passengers.json';
-import { withStyles } from '@material-ui/core/styles';
+import { withStyles, makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
 import Menu, { MenuProps } from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
 import PersonAddOutlinedIcon from '@material-ui/icons/PersonAddOutlined';
@@ -9,6 +10,13 @@ import AddIcon from '@material-ui/icons/Add';
 import RemoveIcon from '@material-ui/icons/Remove';
 import ListItemText from '@material-ui/core/ListItemText';
 
+const useStyles = makeStyles({
+  root: {
+    height: '56px',
+    textTransform: 'none',
+    justifyContent: 'left'
+  },
+});
 
 const StyledMenu = withStyles({
   paper: {
@@ -46,29 +54,44 @@ export default function CustomizedMenus() {
     setAnchorEl(null);
   };
 
-  const passengers = PassengerList.map((passenger, index) => (
+  const onAddPassenger = () => {
+    console.log("ADD");
+  }
+  
+  const onRemovePassenger = () => {
+    console.log("REMOVE");
+  }
+  
+  const classes = useStyles();
+
+  const passengerMenu = PassengerList.map((passenger, index) => (
     <StyledMenuItem key={index}>
       <ListItemText primary={passenger.type} />
-      <Button>
+      <IconButton onClick={onRemovePassenger}>
         <RemoveIcon fontSize="small" />
-      </Button>
+      </IconButton>
       <span className="passenger-count">{passenger.count}</span>
-      <Button>
+      <IconButton onClick={onAddPassenger}>
         <AddIcon fontSize="small" />
-      </Button>         
+      </IconButton>         
     </StyledMenuItem>
   ));
   return (
     <div>
       <Button
+        fullWidth
+        classes={{
+          root: classes.root
+        }}
         aria-controls="customized-menu"
         aria-haspopup="true"
         variant="outlined"
         color="default"
+        size="large"
         onClick={handleClick}
       >
         <PersonAddOutlinedIcon />
-        Passengers
+        Passengers  
       </Button>
       <StyledMenu
         id="customized-menu"
@@ -77,7 +100,7 @@ export default function CustomizedMenus() {
         open={Boolean(anchorEl)}
         onClose={handleClose}
       >
-        {passengers}
+        {passengerMenu}
       </StyledMenu>
     </div>
   );

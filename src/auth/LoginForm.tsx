9b84eae2c39365463b55992a930c44bcc1474d74
Rob <rob.dumont99@gmail.com>
2020-06-02 09:45:54 -0400

    Add form tag

--- src/auth/LoginForm.tsx
+++ src/auth/LoginForm.tsx
@@ -29,38 +29,40 @@ class LoginForm extends React.Component<LoginFormProps> {
         <div className="row">
           <div className="col-sm-10 offset-sm-1">
             <h4>Sign in</h4>
-            <LoginTextField
-              id="login-email"
-              label="Email"
-              variant="outlined"
-              fullWidth
-              required
-              value={this.state.email}
-              onChange={e => this.setState({'email': e.target.value})}/>
-            <LoginTextField
-              id="login-password"
-              label="Password"
-              type="password"
-              variant="outlined"
-              fullWidth
-              required
-              value={this.state.password}
-              onChange={e => this.setState({'password': e.target.value})}/>
-            <div className="row login-button-row">
-              <div className="col-sm-8">
-                <a href="/" className="login-link">Forgot Password?</a>
+            <form>
+              <LoginTextField
+                id="login-email"
+                label="Email"
+                variant="outlined"
+                fullWidth
+                required
+                value={this.state.email}
+                onChange={e => this.setState({'email': e.target.value})}/>
+              <LoginTextField
+                id="login-password"
+                label="Password"
+                type="password"
+                variant="outlined"
+                fullWidth
+                required
+                value={this.state.password}
+                onChange={e => this.setState({'password': e.target.value})}/>
+              <div className="row login-button-row">
+                <div className="col-sm-8">
+                  <a href="/" className="login-link">Forgot Password?</a>
+                </div>
+                <div className="col-sm-4">
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    id="login-button"
+                    size="large"
+                    onClick={this.loginUser}>
+                    Log in
+                  </Button>
+                </div>
               </div>
-              <div className="col-sm-4">
-                <Button
-                  variant="contained"
-                  color="primary"
-                  id="login-button"
-                  size="large"
-                  onClick={this.loginUser}>
-                  Log in
-                </Button>
-              </div>
-            </div>
+            </form>
             <hr/>
             <p>
               Don't have an account yet?

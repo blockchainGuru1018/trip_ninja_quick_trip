90217f4bfef1ef8b0ada6d3ecd4bf1ae5c6953db
Rob <rob.dumont99@gmail.com>
2020-06-02 11:58:01 -0400

    Add button

--- src/auth/MarketingPanel.tsx
+++ src/auth/MarketingPanel.tsx
@@ -1,5 +1,17 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
+import { styled } from '@material-ui/core/styles';
 import marketingImage from '../assets/images/login_marketing_image.png';
+
+const MarketingButton = styled(Button)({
+  margin: '10px 0px',
+  backgroundColor: '#45565E',
+  color: '#ffffff',
+  textTransform: 'none',
+  width: '100%',
+  fontFamily: 'NeuzeitGro-Bol'
+});
+
 
 class MarketingPanel extends React.Component {
   render() {
@@ -7,7 +19,17 @@ class MarketingPanel extends React.Component {
       <div className="row">
         <div className="col-sm-6">
           <h1>How efficient is your booking process?</h1>
-          <h4>Empower travel agents to find the best complex itineraries in seconds, so your team can do more with less.</h4>
+          <div className="row">
+            <div className="col-xl">
+              <h4>Empower travel agents to find the best complex itineraries in seconds, so your team can do more with less.</h4>
+              <MarketingButton
+                variant="contained"
+                id="website-link-button"
+                size="large">
+                See how Trip Ninja increases agent efficiency
+              </MarketingButton>
+            </div>
+          </div>
         </div>
         <div className="col-sm-6">
           <img src={marketingImage} width="100%" alt="" loading="lazy" />

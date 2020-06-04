1e4ee38123454b6773eb5e637fcd23eebd0f8aa2
Rob <rob.dumont99@gmail.com>
2020-06-04 11:50:16 -0400

    remove semicolons

--- src/helpers/DateHelpers.ts
+++ src/helpers/DateHelpers.ts
@@ -3,4 +3,4 @@ export function datesAreOnSameDayOrLater(first: Date, second: Date) {
   return first.getFullYear() >= second.getFullYear() &&
     first.getMonth() >= second.getMonth() &&
     first.getDate() >= second.getDate();
-};
+}

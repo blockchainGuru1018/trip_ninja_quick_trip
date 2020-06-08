dae160435f23ac2bdcb05f0c81101cc2eea79b31
Rob <rob.dumont99@gmail.com>
2020-06-08 11:19:00 -0400

    update hover styling

--- src/App.tsx
+++ src/App.tsx
@@ -54,6 +54,15 @@ const theme = createMuiTheme({
         fontSize: '16px'
       },
     },
+    MuiOutlinedInput: {
+      root: {
+        borderRadius: '2px',
+        "&:hover $notchedOutline": {
+          borderColor: "#CACDD6",
+          borderBottom: 'solid 3px #0DBE7CEB'
+        }
+      }
+    }
   },
 });
 

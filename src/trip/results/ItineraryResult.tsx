ae978fbaa51d822b5596c5f2399bf57b1e0c12c0
Rob <rob.dumont99@gmail.com>
2020-06-10 17:19:00 -0400

    Style change search button

--- src/trip/results/ItineraryResult.tsx
+++ src/trip/results/ItineraryResult.tsx
@@ -3,7 +3,19 @@ import './ItineraryResult.css';
 import ItineraryPath from './ItineraryPath';
 import SegmentNav from './SegmentNav';
 import PricingRequest from './PricingRequest';
+import { CurrencySymbol } from '../../helpers/CurrencySymbolHelper';
 import Button from '@material-ui/core/Button';
+import { styled } from '@material-ui/core/styles';
+
+const ChangeSearchButton = styled(Button)({
+  backgroundColor: '#ffffff',
+  color: '#45565E',
+  border: 'solid 2px #45565E',
+  '&:hover': {
+    backgroundColor: '#45565E',
+    color: '#ffffff',
+  }
+});
 
 class ItineraryResult extends React.Component {
 
@@ -17,18 +29,18 @@ class ItineraryResult extends React.Component {
             </div>
             <div className="col-xl-2 col-lg-3">
               <div className="float-right">
-                <Button
+                <ChangeSearchButton
                   color="secondary"
                   variant="contained"
                   href="/search">
                   Change Search
-                </Button>
+                </ChangeSearchButton>
               </div>
             </div>
           </div>
           <hr/>
           <h1>Your Itinerary</h1>
-          <h4><strong>Total:</strong> $10,000 | 1 ADT</h4>
+          <h4><strong>Total:</strong>{CurrencySymbol("CAD")} $10,000 | 1 ADT</h4>
           <div className="row">
             <div className="col-md-8">
 

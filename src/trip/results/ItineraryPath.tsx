68758b8ae7aa98af5ce784d25ff7f57f39b20aee
Rob <rob.dumont99@gmail.com>
2020-06-10 11:15:39 -0400

    base layout

---
+++ src/trip/results/ItineraryPath.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class ItineraryPath extends React.Component {
+
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+export default ItineraryPath;
--- src/trip/results/ItineraryResult.tsx
+++ src/trip/results/ItineraryResult.tsx
@@ -1,12 +1,34 @@
 import React from 'react';
 import './ItineraryResult.css';
+import ItineraryPath from './ItineraryPath';
+import SegmentNav from './SegmentNav';
 
 class ItineraryResult extends React.Component {
 
   render() {
     return (
-      <div className="row" id="itinerary-result">
+      <div id="itinerary-result">
+        <div className="row">
+          <div className="col-md-10">
+            <ItineraryPath />
+          </div>
+          <div className="col-md-2">
+            <span>Change Search</span>
+          </div>
+        </div>
+        <hr/>
         <h1>Your Itinerary</h1>
+        <div className="row">
+          <h4>Total Price: $10,000 | 1 ADT</h4>
+        </div>
+        <div className="row">
+          <div className="col-sm-3">
+            <SegmentNav />
+          </div>
+          <div className="col-sm-9">
+            
+          </div>
+        </div>
       </div>
     );
   }

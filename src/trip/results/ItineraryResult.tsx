4c8e9fac64c55932e7d35855bb221df8ba9514c6
Rob <rob.dumont99@gmail.com>
2020-06-12 11:20:59 -0400

    segment nav getting path sequence

--- src/trip/results/ItineraryResult.tsx
+++ src/trip/results/ItineraryResult.tsx
@@ -9,11 +9,54 @@ import { CurrencySymbol } from '../../helpers/CurrencySymbolHelper';
 
 class ItineraryResult extends React.Component {
   render() {
-    const trip = {
-      "price": "999.99",
-      "currency": "CAD",
-      "passengers": "1 ADT"
-    };
+    const trip = 
+      {
+        "fare_structure": {
+          "markup": 25,
+          "path_sequence": [
+            "LON-CDG",
+            "CDG-YUL",
+            "YUL-LON",
+            "LON-YHZ"
+          ],
+          "trip_id": "string",
+          "segments": [
+            [
+              {
+                "source": "travelport",
+                "origin": "NYC",
+                "destination": "LHR",
+                "itinerary_type": "ONE_WAY",
+                "itinerary_id": "ad76ah7dhasd76asb76",
+                "itinerary_structure": [],
+                "segment_position": 0,
+                "option_id": "vi65s76sdf9",
+                "option_part": "1/2",
+                "virtual_interline": true,
+                "vi_type": "OPEN_JAW",
+                "vi_position": 1,
+                "weight": 239,
+                "price": 197.1,
+                "base_price": 0,
+                "taxes": 119.1,
+                "fees": 0,
+                "transportation_time": 425,
+                "fare_type": "Published",
+                "baggage": {},
+                "additional_details": {},
+                "cabin_class": "Economic Standard",
+                "alliance": "*A",
+                "private_fare": false,
+                "priced_passengers": [],
+                "segment_time_w_connections": 75,
+                "flights": [],
+                "brands": [ ]
+              }
+            ]
+          ],
+          "flight_details": []
+        }
+      };
     const selectedSegments = 
       <div className="row">
         <div className="col-xl">
@@ -28,9 +71,9 @@ class ItineraryResult extends React.Component {
           <h1 className="your-itinerary">Your Itinerary</h1>
           <h4>
             <strong>Total: </strong> 
-            {CurrencySymbol(trip.currency)}{trip.price}
+            {CurrencySymbol("USD")}21.00
             <span className="divider">|</span>
-            {trip.passengers}
+            1 ADT
           </h4>
           <div className="row">
             <div className="col-md-8">
@@ -42,9 +85,9 @@ class ItineraryResult extends React.Component {
           </div>
         </div>
         
-        <div className="row">
+        <div className="row full-height">
           <div className="col-sm-3">
-            <SegmentNav />
+            <SegmentNav pathSequence={trip.fare_structure.path_sequence}/>
           </div>
           <div className="col-sm-9">
             {selectedSegments}
--- src/trip/results/SegmentNav.tsx
+++ src/trip/results/SegmentNav.tsx
@@ -1,23 +1,21 @@
 import React from 'react';
 
-class SegmentNav extends React.Component {
+interface SegmentNavProps {
+  pathSequence: string[]
+}
 
+class SegmentNav extends React.Component<SegmentNavProps> {
   render() {
-    const pathSequence = [
-      "LON-CDG",
-      "CDG-YUL",
-      "YUL-LON"
-    ];
-    const segments = pathSequence.map((item, index) => (
-      <div className="segment-nav-item">
-        <a className="segment-nav-link" key={index} href={'/results/segment/'+ index}>{item}</a>
+    const segments = this.props.pathSequence.map((item: string, index: number) => (
+      <div className="segment-nav-item" key={index.toString()}>
+        <a className="segment-nav-link" href={'/results/segment/'+ index}>{item}</a>
       </div>
     ));
     return (
       <div id="segment-nav">
         <h3>Itinerary</h3>
         <div className="segment-nav-item">
-          <a className="segment-nav-link active" href="/results/itinerary">Overview</a>
+          <a className="segment-nav-link active" href="/results/itinerary/" key="overview">Overview</a>
         </div>
         {segments}
       </div>

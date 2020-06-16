23952be3572bc08c52d63b8abae16f90bbff2cb6
Rob <rob.dumont99@gmail.com>
2020-06-16 13:08:42 -0400

    Remove unused import

--- src/actions/ResultsActions.ts
+++ src/actions/ResultsActions.ts
@@ -1,27 +1,26 @@
-import API from '../Api';
 import { ResultsDetails } from '../trip/results/ResultsInterfaces';
 
 export const setSearchResults = (results: ResultsDetails) => (dispatch: any) => {
-  dispatch(setResults(results))
-}
+  dispatch(setResults(results));
+};
 
 export function setResults(results: ResultsDetails) {
   return {
     type: 'SET_RESULTS',
     results
-  }
+  };
 }
 
 export function setErrorDetails(value: boolean) {
   return {
     type: 'SET_ERROR_DETAILS',
     value
-  }
+  };
 }
 
 export function setTripType(value: string) {
   return {
     type: 'SET_TRIP_TYPE',
     value
-  }
+  };
 }

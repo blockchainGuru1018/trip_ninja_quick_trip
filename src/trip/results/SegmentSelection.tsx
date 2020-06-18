0e94621dbf533004213e8599cbc31497f99b8dbf
Rob <rob.dumont99@gmail.com>
2020-06-18 12:54:27 -0400

    add in base layout

--- src/trip/results/SegmentSelection.tsx
+++ src/trip/results/SegmentSelection.tsx
@@ -1,12 +1,43 @@
 import React from 'react';
+import SegmentNav from './SegmentNav';
+import ResultsHeader from './ResultsHeader';
 import './SegmentSelection.css';
+import FlightIcon from '@material-ui/icons/Flight';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  useParams
+} from "react-router-dom";
 
-class SegmentSelection extends React.Component {
+
+interface SegmentSelectionProps {
+
+}
+
+class SegmentSelection extends React.Component<SegmentSelectionProps> {
 
   render() {
+    let index = this.props;
+    console.log(index);
     return (
       <div className="row" id="segment-selection">
-        <h1>Select Segments</h1>
+        <div className="itinerary-header">
+          <ResultsHeader segments={selectedTrip} flights={trip.flight_details}/>
+          <h1>
+            LGW
+            <FlightIcon color="primary" className="rotate-90"/>
+            PAR
+          </h1>
+        </div>
+        <div className="row">
+          <div className="col-md-2 no-padding">
+            <SegmentNav pathSequence={trip.path_sequence}/>
+          </div>
+          <div className="col-md-10 segment-list">
+
+          </div>
+        </div>
       </div>
     );
   }

f63abad6c3dd32a478e6d78d18117f955e7e7eac
Rob <rob.dumont99@gmail.com>
2020-06-19 13:13:21 -0400

    Update SegmentNav.tsx

--- src/trip/results/SegmentNav.tsx
+++ src/trip/results/SegmentNav.tsx
@@ -10,7 +10,7 @@ class SegmentNav extends React.Component<SegmentNavProps> {
   render() {
     const segments = this.props.pathSequence.map((item: string, index: number) => (
       <div className="segment-nav-item" key={index.toString()}>
-        <button className={ 'segment-nav-link ' + (this.props.currentIndex == index ? 'active' : '')}
+        <button className={'segment-nav-link ' + (this.props.currentIndex === index ? 'active' : '')}
           onClick={() => history.push('/results/segment/'+ index)}>
           {item}
         </button>
@@ -20,7 +20,7 @@ class SegmentNav extends React.Component<SegmentNavProps> {
       <div id="segment-nav">
         <h3>Itinerary</h3>
         <div className="segment-nav-item">
-          <button className={ 'segment-nav-link ' + (!this.props.currentIndex ? 'active' : '')}
+          <button className={'segment-nav-link ' + (typeof this.props.currentIndex === 'undefined' ? 'active' : '')}
             onClick={() => history.push('/results/itinerary/')} 
             key="overview">
             Overview

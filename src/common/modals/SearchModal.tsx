865bac2a324ebf7f334034b5862b766b785fccc8
Rob <rob.dumont99@gmail.com>
2020-07-07 12:18:08 -0400

    refactor modals

--- src/common/modals/SearchModal.tsx
+++ src/common/modals/SearchModal.tsx
@@ -1,37 +1,9 @@
 import React from 'react';
-import { makeStyles, createStyles } from '@material-ui/core/styles';
-import Modal from '@material-ui/core/Modal';
-import Backdrop from '@material-ui/core/Backdrop';
-import Fade from '@material-ui/core/Fade';
-import { useEffect } from 'react';
-import CircularProgress from '@material-ui/core/CircularProgress';
 import FlightPath from '../../trip/search/TripPath';
 import { Flight } from '../../trip/search/SearchInterfaces';
+import DefaultModal from './DefaultModal';
 import './Modals.css';
 
-const useStyles = makeStyles(() =>
-  createStyles({
-    modal: {
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-      height: '100vh'
-    },
-    paper: {
-      background: '#FFFFFF 0% 0% no-repeat padding-box',
-      boxShadow: '0px 3px 6px #00000029',
-      border: '1px solid #ECEEEF',
-      borderRadius: '5px',
-      top: '142px',
-      left: '130px',
-      width: '800px',
-      height: 'auto'
-    },
-    backDrop: {
-      height: '100vh'
-    }
-  }),
-);
 
 interface SearchModalProps {
   loading: boolean
@@ -39,34 +11,15 @@ interface SearchModalProps {
 }
 
 export default function SearchModal(props: SearchModalProps) {
-  const classes = useStyles();
-  const [open, setOpen] = React.useState(false);
-
-  useEffect(() => setOpen(props.loading), [props.loading]);
-
+  const primaryText = "Finding the best route and flight options";
+  const secondaryText = <FlightPath flights={props.flights}/>;
   return (
     <div>
-      <Modal
-        aria-labelledby="transition-modal-title"
-        aria-describedby="transition-modal-description"
-        className={classes.modal}
-        open={open}
-        closeAfterTransition
-        BackdropComponent={() => <Backdrop open={open} className={classes.backDrop} />}
-        BackdropProps={{
-          timeout: 500,
-        }}
-      >
-        <Fade in={open}>
-          <div className={classes.paper + ' centered-container search-modal-container'}>
-            <h2 id="transition-modal-title" className='search-modal-title'>Finding the best route and flight options</h2>
-            <div>
-              <CircularProgress />
-            </div>
-            <FlightPath flights={props.flights}/>
-          </div>
-        </Fade>
-      </Modal>
+      <DefaultModal 
+        loading={props.loading}
+        primaryText={primaryText}
+        secondaryText={secondaryText}
+      />
     </div>
   );
 }

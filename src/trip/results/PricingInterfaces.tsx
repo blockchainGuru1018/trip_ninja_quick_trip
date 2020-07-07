ecc1cfb629cc964c6611bfcb9f9e9421680eb603
Rob <rob.dumont99@gmail.com>
2020-07-07 13:10:24 -0400

    fix pricing interface spacing

--- src/trip/results/PricingInterfaces.tsx
+++ src/trip/results/PricingInterfaces.tsx
@@ -1,14 +1,14 @@
 
 
 export interface PricingDetails {
-    trip_id: string;
-    trip_type: string;
-    currency: string;
-    price: number;
-    markup?: number;
-    source: string;
-    itineraries?: Array<object>;
-    loading: boolean;
+  trip_id: string;
+  trip_type: string;
+  currency: string;
+  price: number;
+  markup?: number;
+  source: string;
+  itineraries?: Array<object>;
+  loading: boolean;
   }
 
 export const defaultPricingDetails: PricingDetails = {
@@ -21,37 +21,37 @@ export const defaultPricingDetails: PricingDetails = {
 };
 
 export interface Itineraries {
-    itinerary_reference: number;
-    traveller_list: Array<string>;
-    plating_carrier?: string;
-    credentials: Credentials;
-    itinerary_type: string;
-    segments: Array<FlightSegment>;
+  itinerary_reference: number;
+  traveller_list: Array<string>;
+  plating_carrier?: string;
+  credentials: Credentials;
+  itinerary_type: string;
+  segments: Array<FlightSegment>;
 }
 
 
 export interface FlightSegment{
-    segment_id: string;
-    flights: Array<Flight>
+  segment_id: string;
+  flights: Array<Flight>
 }
 
 export interface Flight{
-    key: string;
-    origin: string;
-    destination: string;
-    booking_code: string;
-    cabin_class?: string;
-    carrier: string;
-    flight_time?: number;
-    flight_number: string;
-    departure_time: string;
-    arrival_time?: string;
-    brand_identifier?: string;
+  key: string;
+  origin: string;
+  destination: string;
+  booking_code: string;
+  cabin_class?: string;
+  carrier: string;
+  flight_time?: number;
+  flight_number: string;
+  departure_time: string;
+  arrival_time?: string;
+  brand_identifier?: string;
 }
 
 export interface Credentials{
-    data_source: string;
-    pcc?: string;
-    provider?: string;
-    region?: string;
+  data_source: string;
+  pcc?: string;
+  provider?: string;
+  region?: string;
 }

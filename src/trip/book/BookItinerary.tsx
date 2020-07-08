2147250109ec004ced8bf9c147332e4259b19b57
Andres Collart <andres.collart@tripninja.io>
2020-07-08 11:34:05 -0300

    initial setup

--- src/trip/book/BookItinerary.tsx
+++ src/trip/book/BookItinerary.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import SegmentPreviews from '../results/SegmentPreviews';
 
-interface ItineraryProps {
+interface BookItineraryProps {
 
 }
 
-class Itinerary extends React.Component<ItineraryProps> {
+class BookItinerary extends React.Component<BookItineraryProps> {
 
   render() {
 
@@ -20,4 +21,4 @@ class Itinerary extends React.Component<ItineraryProps> {
 
 }
 
-export default Itinerary;
+export default BookItinerary;

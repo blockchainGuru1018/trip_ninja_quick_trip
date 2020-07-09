eddf4279a11b1cd930a587982bfc2cec13eae3e8
Rob <rob.dumont99@gmail.com>
2020-07-09 14:43:45 -0400

    add country select and country list

--- src/trip/book/PassengerCountrySelect.tsx
+++ src/trip/book/PassengerCountrySelect.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import MenuItem from '@material-ui/core/MenuItem';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
+import CountryList from '../../assets/data/countries.json';
 
 interface PassengerCountrySelectProps {
 
@@ -10,6 +11,10 @@ interface PassengerCountrySelectProps {
 class PassengerCountrySelect extends React.Component<PassengerCountrySelectProps> {
 
   render() {
+    const countries = CountryList.map((country, index) => (
+      <MenuItem key={index} value={country.code}>{country.name}</MenuItem>
+    ));
+
     return (
       <FormControl fullWidth>
         <Select
@@ -17,8 +22,7 @@ class PassengerCountrySelect extends React.Component<PassengerCountrySelectProps
           value=""
           variant="outlined"
         >
-          <MenuItem value="CA">Country</MenuItem>
-          <MenuItem value="M">Canada</MenuItem>
+          {countries}
         </Select>
       </FormControl>
     );

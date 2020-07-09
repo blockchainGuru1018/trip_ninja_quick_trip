d9c9ab1af8e83034bda10546f3e4a469e4494242
Andres Collart <andres.collart@tripninja.io>
2020-07-09 11:06:26 -0300

    fixed code smell

--- src/helpers/FlightDetailsHelper.ts
+++ src/helpers/FlightDetailsHelper.ts
@@ -3,8 +3,8 @@ import { Segment, FlightResultsDetails} from '../trip/results/ResultsInterfaces'
 export function getFlightDetailsBySegment(segment: Segment, flightDetails: Array<FlightResultsDetails>){
   return segment.flights.map((flight: any) => {
     const filteredFlightDetails = flightDetails.filter(
-      (flightDetails: FlightResultsDetails) =>
-        flight.flight_detail_ref === flightDetails.reference
+      (flightResultsDetails: FlightResultsDetails) =>
+        flight.flight_detail_ref === flightResultsDetails.reference
     );
     return filteredFlightDetails[0];
   }

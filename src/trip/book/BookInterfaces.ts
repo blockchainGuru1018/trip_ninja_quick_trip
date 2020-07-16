61084d08605a541901da6c246b7bfc6ce6fc4298
Rob Dumont <rob.dumont99@gmail.com>
2020-07-16 13:59:54 -0400

    set to use passenger code

--- src/trip/book/BookInterfaces.ts
+++ src/trip/book/BookInterfaces.ts
@@ -21,6 +21,7 @@ export interface PassengerInfo {
   passport_number?: string;
   passport_expiration?: string;
   passenger_type: string;
+  passenger_type_name: string;
   email?: string;
   updated: boolean;
 }
@@ -40,7 +41,8 @@ export interface PaymentInfo {
 }
 
 export const defaultPassengerInfo: PassengerInfo = {
-  'passenger_type': 'Adult',
+  'passenger_type': 'ADT',
+  'passenger_type_name': 'Adult',
   'first_name': '',
   'last_name': '',
   'date_of_birth': new Date().toISOString().slice(0,10),

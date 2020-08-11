297e42044db953ef98b0b2611401a7d8e052c2fd
k.mcclymont <kieran.mcclymont@tripninja.io>
2020-08-11 12:53:57 -0300

    - fixed code smell

--- src/bookings/BookingDetailsDrawer.tsx
+++ src/bookings/BookingDetailsDrawer.tsx
@@ -9,7 +9,6 @@ import IconButton from "@material-ui/core/IconButton";
 import FareBreakdown from '../common/FareBreakdown';
 import PassengerDetails from './PassengerDetails';
 import ManageBooking from './ManageBooking';
-import Tooltip from '@material-ui/core/Tooltip';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import { Booking, PnrInfo } from './BookingsInterfaces';
 import ItineraryDetails from '../common/ItineraryDetails';

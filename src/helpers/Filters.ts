123b0035f25b36f1e02fa71804da3688216d6812
Andres Collart <andres.collart@tripninja.io>
2020-08-12 09:20:44 -0300

    setting up new filters

--- src/helpers/Filters.ts
+++ src/helpers/Filters.ts
@@ -1,24 +1,59 @@
 import { Segment, Filters } from '../trip/results/ResultsInterfaces';
+import { cloneDeep } from 'lodash';
 
 const resetFilters = (segments: Array<Segment>) =>
   segments.forEach((segment: Segment) =>
     segment.filtered = false
   );
 
-export const filterSegments = (segments: Array<Segment>, filters: Filters) => {
-  let totalFiltered = 0;
+export const baggageFilter = (segments: Array<Segment>, filterValue: number | undefined) => {
+  let totalNotFiltered = 0;
+
+  let segments_copy = cloneDeep(segments);
 
   segments.forEach((segment: Segment) => {
-    segment.filtered =
-      (segment.flights.length - 1 > filters.numberOfStops!) || // numberOfStops Filter
-      (filters.baggage > 0 && segment.baggage.number_of_pieces < filters.baggage); //baggage Filter
-    if (!segment.filtered) {
-      totalFiltered += 1;
+    if (!segment.filtered && filterValue! > 0) {
+      segment.filtered = segment.baggage.number_of_pieces < filterValue!;
+      if (!segment.filtered) {
+        totalNotFiltered += 1;
+      }
     }
   });
-  if (totalFiltered === 0) {
-    resetFilters(segments);
+
+  if (totalNotFiltered === 0) {
+    segments = cloneDeep(segments_copy);
   }
+};
+
+export const numberOfStopsFilter = (segments: Array<Segment>, filterValue: number | undefined) => {
+  let totalNotFiltered = 0;
+
+  let segments_copy = cloneDeep(segments);
+
+  segments.forEach((segment: Segment) => {
+    if (!segment.filtered) {
+      segment.filtered = segment.flights.length - 1 > filterValue!;
+      if (!segment.filtered) {
+        totalNotFiltered += 1;
+      }
+    }
+  });
+
+  if (totalNotFiltered === 0) {
+    segments = cloneDeep(segments_copy);
+  }
+};
+
+export const filterSegments = (segments: Array<Segment>, filters: Filters) => {
+  const filtersKeys = Object.keys(filters);
+  resetFilters(segments);
+
+  //if (filtersKeys.includes('baggage')) {
+  //  baggageFilter(segments, filters.baggage);
+  //}
+  //if (filtersKeys.includes('numberOfStops')) {
+  //  numberOfStopsFilter(segments, filters.numberOfStops);
+  //}
 
   return segments;
 };

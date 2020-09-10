8aa89f6fdadfb521e73321d3d9d09477709c191f
k.mcclymont <kieran.mcclymont@tripninja.io>
2020-09-10 14:05:47 -0300

    - fixed the passenger details

--- src/bookings/PassengerDetails.tsx
+++ src/bookings/PassengerDetails.tsx
@@ -26,10 +26,10 @@ class PassengerDetails extends React.Component<PassengerDetailsProps> {
         <div className="col-sm-2 no-pad-left my-auto">
           <div className="float-left passenger-icon">
             <PersonOutlineIcon color="primary" fontSize="large" />
+            <p className="text-small text-center">{passenger.passenger_type}</p>
           </div>
           <div>
             <p className="text-bold">{passenger.first_name} {passenger.last_name}</p>
-            <p className="text-small">{passenger.passenger_type}</p>
           </div>
         </div>
         <div className="col-sm-2 border-left">
@@ -42,15 +42,15 @@ class PassengerDetails extends React.Component<PassengerDetailsProps> {
           <p>{this.getPassengerGender(passenger.gender)}</p>
           <p>{passenger.passport_country ? this.getCountryName(passenger.passport_country): '-'}</p>
           <p>{passenger.passport_number ? passenger.passport_number : '-'}</p>
-          <p><Moment format="DD/MM/YYYY">{passenger.passport_expiration ? passenger.passport_expiration : '-'}</Moment></p>
+          <p>{passenger.passport_expiration ? <Moment format="DD/MM/YYYY">{passenger.passport_expiration}</Moment> : '-'}</p>
         </div>
         <div className="col-sm-1">
           <p className="passenger-field">Phone</p>
           <p className="passenger-field">Email</p>
         </div>
         <div className="col-sm-2 no-pad-left">
-          <p>{passenger.phone_number}</p>
-          <p>{passenger.email}</p>
+          <p>{passenger.phone_number ? passenger.phone_number : '-'}</p>
+          <p>{passenger.email ? passenger.email : '-'}</p>
         </div>
       </div>
     ));

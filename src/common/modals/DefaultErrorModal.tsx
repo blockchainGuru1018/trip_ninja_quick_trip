06fd0914423f972902dc6f92fb9e27f6645043b9
Rob Dumont <rob.dumont99@gmail.com>
2020-10-02 13:37:54 -0400

    remove duplicated styling

--- src/common/modals/DefaultErrorModal.tsx
+++ src/common/modals/DefaultErrorModal.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { makeStyles, createStyles } from '@material-ui/core/styles';
 import Modal from '@material-ui/core/Modal';
+import Button from '@material-ui/core/Button';
 import { useEffect } from 'react';
 import { Errors } from '../../trip/results/ResultsInterfaces';
 import './Modals.css';
@@ -11,6 +12,7 @@ import QueueingErrorModal from './QueueingErrorModal';
 import CancellingErrorModal from './CancellingErrorModal';
 import TicketingErrorModal from './TicketingErrorModal';
 import { setErrorDetails } from '../../actions/ResultsActions';
+import { useTranslation } from 'react-i18next';
 
 const useStyles = makeStyles(() =>
   createStyles({
@@ -19,6 +21,16 @@ const useStyles = makeStyles(() =>
       alignItems: 'center',
       justifyContent: 'center',
       height: '100vh'
+    },
+    paper: {
+      background: '#FFFFFF 0% 0% no-repeat padding-box',
+      boxShadow: '0px 3px 6px #00000029',
+      border: '1px solid #ECEEEF',
+      borderRadius: '5px',
+      top: '142px',
+      left: '130px',
+      width: '640px',
+      height: '336px'
     }
   }),
 );
@@ -31,6 +43,7 @@ interface DefaultErrorModalProps {
 export default function DefaultErrorModal(props: DefaultErrorModalProps) {
   const classes = useStyles();
   const [open, setOpen] = React.useState(false);
+  const [ t ] = useTranslation('common');
   useEffect(() => setOpen(props.errors.errorFound), [props.errors.errorFound]);
 
   const errorModalMap = new Map ()
@@ -50,7 +63,9 @@ export default function DefaultErrorModal(props: DefaultErrorModalProps) {
         open={open}
         onClose={() => props.setErrorDetails(false, props.errors.errorType)}
       >
-        {props.errors.errorType ? errorModalMap.get(props.errors.errorType) : <div></div>}
+        <div className={classes.paper + ' centered-container'}>
+          {props.errors.errorType ? errorModalMap.get(props.errors.errorType) : <div></div>}
+        </div>        
       </Modal>
     </div>
   );
--- src/common/modals/TicketingErrorModal.tsx
+++ src/common/modals/TicketingErrorModal.tsx
@@ -1,37 +1,20 @@
 import React from 'react';
-import { makeStyles, createStyles } from '@material-ui/core/styles';
 import './Modals.css';
 import Button from '@material-ui/core/Button';
 import { setErrorDetails } from '../../actions/ResultsActions';
 import { useTranslation } from 'react-i18next';
-
-const useStyles = makeStyles(() =>
-  createStyles({
-    paper: {
-      background: '#FFFFFF 0% 0% no-repeat padding-box',
-      boxShadow: '0px 3px 6px #00000029',
-      border: '1px solid #ECEEEF',
-      borderRadius: '5px',
-      top: '142px',
-      left: '130px',
-      width: '640px',
-      height: '250px'
-    }
-  }),
-);
 
 interface TicketingErrorModalProps {
   setErrorDetails: typeof setErrorDetails;
 }
 
 export default function TicketingErrorModal(props: TicketingErrorModalProps) {
-  const classes = useStyles();
   const [ t ] = useTranslation('common');
 
   return (
-    <div className={classes.paper + ' centered-container'}>
-      <h2 id="transition-modal-title" className='search-modal-title'>{t('common.modals.ticketingErrorModal.title')}</h2>
-      <div className='search-modal-text-container'>
+    <div>
+      <h2 id="transition-modal-title" className='modal-title'>{t('common.modals.ticketingErrorModal.title')}</h2>
+      <div className='modal-text-container'>
         <p>
           {t('common.modals.ticketingErrorModal.body')}
         </p>

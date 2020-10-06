5fe1446cd37b62bace31069257e260a5b2f19764
Rob Dumont <rob.dumont99@gmail.com>
2020-10-06 15:58:11 -0400

    duplication refactor

---
+++ src/trip/results/VirtualInterlineSegment.tsx
@@ -0,0 +1,103 @@
+import React from 'react';
+import { Segment, FlightResultsDetails, Results } from './ResultsInterfaces';
+import SegmentPreviewDetails from './SegmentPreviewDetails';
+import IconButton from '@material-ui/core/IconButton';
+import Fade from '@material-ui/core/Fade';
+import FlightLogo from '../../common/FlightLogo';
+import FlightTime from '../../common/FlightTime';
+import SegmentBaggage from '../../common/SegmentBaggage';
+import FlightStops from '../../common/FlightStops';
+import FlightTypes from '../../common/FlightTypes';
+import SegmentOriginDestination from '../../common/SegmentOriginDestination';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import { updateActives, updateFareFamily, getTravelportBrands } from '../../actions/ResultsActions';
+import expandedIcon from '../../assets/images/expanded_icon.svg';
+
+
+interface VirtualInterlineSegmentProps {
+  index: number;
+  segment: Segment;
+  segmentFlightDetails: Array<FlightResultsDetails>;
+  segmentSelect: boolean;
+  activeSegment?: Segment;
+  segmentOptionsIndex?: number;
+  currency: string;
+  updateActives?: typeof updateActives;
+  updateFareFamily?: typeof updateFareFamily;
+  getTravelportBrands?: typeof getTravelportBrands;
+  totalPrice: number;
+  trip: Results;
+}
+
+class VirtualInterlineSegment extends React.Component<VirtualInterlineSegmentProps> {
+
+  state = {
+    expandedSegment: -1
+  }
+
+  render() {
+    const open = this.state.expandedSegment === this.props.index;
+
+    return(
+      <div>        
+        <div className="row">
+          <div className="col-xl-1 my-auto">
+            <img src={expandedIcon} alt="vi-segment-icon" className="vi-segment-icon" />
+          </div>
+          <div className="col-xl-11">
+            <div className="row segment">
+              {!this.props.segmentSelect
+              && <SegmentOriginDestination segment={this.props.segment} departure={this.props.segmentFlightDetails[0].departure_time} />
+              }
+              <FlightLogo flights={this.props.segmentFlightDetails} />
+              <FlightTime flights={this.props.segmentFlightDetails} />
+              <FlightStops flights={this.props.segmentFlightDetails} viParent={false}/>
+              <FlightTypes segment={this.props.segment} />
+              <SegmentBaggage baggage={this.props.segment.baggage.number_of_pieces} />
+              <div className={(this.props.segmentSelect ? 'offset-sm-2 ' : '') + 'col-sm-1 icon-expand-preview my-auto'}>
+                <IconButton
+                  className={'expand-icon' + (open ? ' rotated-180' : '')}
+                  onClick={(() =>
+                    this.state.expandedSegment === 0
+                      ? this.setState({expandedSegment: -1})
+                      : this.setState({expandedSegment: this.props.index})
+                  )}
+                >
+                  <ExpandMoreIcon fontSize="large" style={{ color: 'var(--tertiary)' }}/>
+                </IconButton>
+              </div>
+            </div>
+            <Fade
+              in={open}
+              style={{display: open ? 'block' : 'none', width: '100%'}}>
+              <div>
+                { open
+                && <SegmentPreviewDetails
+                  segmentOptionsIndex={this.props.segmentOptionsIndex}
+                  segment={this.props.segment}
+                  flightDetails={this.props.segmentFlightDetails}
+                  currency={this.props.currency}
+                  segmentSelect={this.props.segmentSelect}
+                  updateActives={this.props.updateActives}
+                  closeAllDropDowns={this.closeAllDropDowns}
+                  updateFareFamily={this.props.updateFareFamily}
+                  totalPrice={this.props.totalPrice}
+                  activeSegment={this.props.activeSegment}
+                  getTravelportBrands={this.props.getTravelportBrands}
+                  trip={this.props.trip}
+                  viParent={false}
+                />
+                }
+              </div>
+            </Fade>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  closeAllDropDowns = () => this.setState({expandedSegment: -1})
+
+}
+
+export default VirtualInterlineSegment;
--- src/trip/results/VirtualInterlineSegments.tsx
+++ src/trip/results/VirtualInterlineSegments.tsx
@@ -1,18 +1,8 @@
 import React from 'react';
 import { Segment, FlightResultsDetails, Results } from './ResultsInterfaces';
-import SegmentPreviewDetails from './SegmentPreviewDetails';
-import IconButton from '@material-ui/core/IconButton';
-import Fade from '@material-ui/core/Fade';
-import FlightLogo from '../../common/FlightLogo';
-import FlightTime from '../../common/FlightTime';
-import SegmentBaggage from '../../common/SegmentBaggage';
-import FlightStops from '../../common/FlightStops';
-import FlightTypes from '../../common/FlightTypes';
 import SelfTransferLabel from '../../common/SelfTransferLabel';
-import SegmentOriginDestination from '../../common/SegmentOriginDestination';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import VirtualInterlineSegment from './VirtualInterlineSegment';
 import { updateActives, updateFareFamily, getTravelportBrands } from '../../actions/ResultsActions';
-import expandedIcon from '../../assets/images/expanded_icon.svg';
 
 
 interface VirtualInterlineSegmentsProps {
@@ -36,135 +26,52 @@ interface VirtualInterlineSegmentsProps {
 
 class VirtualInterlineSegments extends React.Component<VirtualInterlineSegmentsProps> {
 
-  state = {
-    expandedSegment: -1
-  }
-
   render() {
-    const openViPosition0 = this.state.expandedSegment === 0;
-    const openViPosition1 = this.state.expandedSegment === 1;
     let flightDetails = this.props.segmentFlightDetails.concat(this.props.viLinkedSegmentFlightDetails && this.props.segment.virtual_interline ? this.props.viLinkedSegmentFlightDetails : []);
 
     return(
       <div>        
         <div className="vi-segment-container">
-          <div className="row">
-            <div className="col-xl-1 my-auto">
-              <img src={expandedIcon} alt="vi-segment-icon" className="vi-segment-icon" />
-            </div>
-            <div className="col-xl-11">
-              <div className="row segment">
-                {!this.props.segmentSelect
-                && <SegmentOriginDestination segment={this.props.segment} departure={this.props.segmentFlightDetails[0].departure_time} />
-                }
-                <FlightLogo flights={this.props.segmentFlightDetails} />
-                <FlightTime flights={this.props.segmentFlightDetails} />
-                <FlightStops flights={this.props.segmentFlightDetails} viParent={false}/>
-                <FlightTypes segment={this.props.segment} />
-                <SegmentBaggage baggage={this.props.segment.baggage.number_of_pieces} />
-                <div className={(this.props.segmentSelect ? 'offset-sm-2 ' : '') + 'col-sm-1 icon-expand-preview my-auto'}>
-                  <IconButton
-                    className={'expand-icon' + (openViPosition0 ? ' rotated-180' : '')}
-                    onClick={(() =>
-                      this.state.expandedSegment === 0
-                        ? this.setState({expandedSegment: -1})
-                        : this.setState({expandedSegment: 0})
-                    )}
-                  >
-                    <ExpandMoreIcon fontSize="large" style={{ color: 'var(--tertiary)' }}/>
-                  </IconButton>
-                </div>
-              </div>
-              <Fade
-                in={openViPosition0}
-                style={{display: openViPosition0 ? 'block' : 'none', width: '100%'}}>
-                <div>
-                  { openViPosition0
-                  && <SegmentPreviewDetails
-                    segmentOptionsIndex={this.props.segmentOptionsIndex}
-                    segment={this.props.segment}
-                    flightDetails={this.props.segmentFlightDetails}
-                    currency={this.props.currency}
-                    segmentSelect={this.props.segmentSelect}
-                    updateActives={this.props.updateActives}
-                    closeAllDropDowns={this.closeAllDropDowns}
-                    updateFareFamily={this.props.updateFareFamily}
-                    totalPrice={this.props.totalPrice}
-                    activeSegment={this.props.activeSegment}
-                    getTravelportBrands={this.props.getTravelportBrands}
-                    trip={this.props.trip}
-                    viParent={false}
-                  />
-                  }
-                </div>
-              </Fade>
-            </div>
-          </div>
+          <VirtualInterlineSegment
+            segment={this.props.segment}
+            index={1000}
+            key={this.props.index}
+            segmentFlightDetails={this.props.segmentFlightDetails}
+            segmentSelect={this.props.segmentSelect}
+            activeSegment={this.props.activeSegment}
+            currency={this.props.currency}
+            segmentOptionsIndex={this.props.segmentOptionsIndex}
+            updateActives={this.props.updateActives}
+            updateFareFamily={this.props.updateFareFamily}
+            totalPrice={this.props.totalPrice}
+            getTravelportBrands={this.props.getTravelportBrands}
+            trip={this.props.trip}
+          />
           <SelfTransferLabel 
             destinationName={this.props.segment.destination_name}
             flights={flightDetails}
           />
           { this.props.viLinkedSegment && this.props.viLinkedSegmentFlightDetails &&
-            <div className="row">
-              <div className="col-xl-1 my-auto">
-                <img src={expandedIcon} alt="vi-segment-icon" className="vi-segment-icon" />
-              </div>
-              <div className="col-xl-11">
-                <div className="row segment">
-                  {!this.props.segmentSelect
-                  && <SegmentOriginDestination segment={this.props.viLinkedSegment} departure={this.props.viLinkedSegmentFlightDetails[0].departure_time} />
-                  }
-                  <FlightLogo flights={this.props.viLinkedSegmentFlightDetails} />
-                  <FlightTime flights={this.props.viLinkedSegmentFlightDetails} />
-                  <FlightStops flights={this.props.viLinkedSegmentFlightDetails} viParent={false} />
-                  <FlightTypes segment={this.props.viLinkedSegment} />
-                  <SegmentBaggage baggage={this.props.viLinkedSegment.baggage.number_of_pieces} />
-                  <div className={(this.props.segmentSelect ? 'offset-sm-2 ' : '') + 'col-sm-1 icon-expand-preview my-auto'}>
-                    <IconButton
-                      className={'expand-icon' + (openViPosition1 ? ' rotated-180' : '')}
-                      onClick={(() =>
-                        this.state.expandedSegment === 1
-                          ? this.setState({expandedSegment: -1})
-                          : this.setState({expandedSegment: 1})
-                      )}
-                    >
-                      <ExpandMoreIcon fontSize="large" style={{ color: 'var(--tertiary)' }}/>
-                    </IconButton>
-                  </div>
-                </div>
-                <Fade
-                  in={openViPosition1}
-                  style={{display: openViPosition1 ? 'block' : 'none', width: '100%'}}>
-                  <div>
-                    { openViPosition1
-                    && <SegmentPreviewDetails
-                      segmentOptionsIndex={this.props.segmentOptionsIndex}
-                      segment={this.props.viLinkedSegment}
-                      flightDetails={this.props.viLinkedSegmentFlightDetails}
-                      currency={this.props.currency}
-                      segmentSelect={this.props.segmentSelect}
-                      updateActives={this.props.updateActives}
-                      closeAllDropDowns={this.closeAllDropDowns}
-                      updateFareFamily={this.props.updateFareFamily}
-                      totalPrice={this.props.totalPrice}
-                      activeSegment={this.props.activeSegment}
-                      getTravelportBrands={this.props.getTravelportBrands}
-                      trip={this.props.trip}
-                      viParent={false}
-                    />
-                    }
-                  </div>
-                </Fade>
-              </div>
-            </div>
+            <VirtualInterlineSegment
+              segment={this.props.viLinkedSegment}
+              index={1001}
+              key={this.props.index}
+              segmentFlightDetails={this.props.viLinkedSegmentFlightDetails}
+              segmentSelect={this.props.segmentSelect}
+              activeSegment={this.props.activeSegment}
+              currency={this.props.currency}
+              segmentOptionsIndex={this.props.segmentOptionsIndex}
+              updateActives={this.props.updateActives}
+              updateFareFamily={this.props.updateFareFamily}
+              totalPrice={this.props.totalPrice}
+              getTravelportBrands={this.props.getTravelportBrands}
+              trip={this.props.trip}
+            />
           }
         </div>
       </div>
     );
   }
-
-  closeAllDropDowns = () => this.setState({expandedSegment: -1})
-
 }
 
 export default VirtualInterlineSegments;

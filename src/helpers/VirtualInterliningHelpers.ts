f0e03eefc5775c3d4b35646be0ea896f78f3a3ce
Rob Dumont <rob.dumont99@gmail.com>
2020-10-07 19:55:14 -0400

    updates

--- src/helpers/VirtualInterliningHelpers.ts
+++ src/helpers/VirtualInterliningHelpers.ts
@@ -4,7 +4,7 @@ import { Results, Segment } from "../trip/results/ResultsInterfaces";
 export function getFullTripWithVi(actives: Array<Segment>, trip: Results) {
   let fullTrip: Array<Segment> = [];
   actives.forEach((activeSegment: Segment, segmentIndex: number) => {
-    fullTrip.push(activeSegment);
+    //fullTrip.push(activeSegment);
     if (activeSegment.virtual_interline) {
       const viSolutionId = activeSegment.vi_solution_id;
       const viSolutionSegment: Segment | undefined = trip.segments[segmentIndex].find((segment: Segment) =>

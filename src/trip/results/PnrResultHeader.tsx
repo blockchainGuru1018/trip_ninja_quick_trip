7faf37bdc63f9c5284a8cc0671dad02114079f60
Rob Dumont <rob.dumont99@gmail.com>
2020-10-29 16:08:08 -0400

    display the green segment indicators

--- src/trip/results/PnrResultHeader.tsx
+++ src/trip/results/PnrResultHeader.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { currencySymbol } from '../../helpers/CurrencySymbolHelper';
 import { itineraryTypeMap } from '../../helpers/ItineraryTypeHelper';
-import Itinerary from '../book/Itinerary';
+
 
 interface PnrResultHeaderProps {
   itineraryNumber: number;
@@ -10,26 +10,29 @@ interface PnrResultHeaderProps {
   currency: string;
   segmentType: string;
   segmentCount: number;
+  structure: Array<Number>;
 }
 
 export default function PnrResultHeader(props: PnrResultHeaderProps) {
   const [ t ] = useTranslation('common');
 
   const segmentIndicators = () => {
-    let indicatorGroup = "";
-    for (let i=1; i++; i<=props.segmentCount) {
-      indicatorGroup += <span className={i === props.itineraryNumber ? 'current-segment-indicator' : 'segment-indicator'}></span>;    
+    let indicatorGroup = [];
+    for (let i=0; i<props.segmentCount; i++) {
+      indicatorGroup.push(<div className={'col ' + (props.structure.includes(i) ? 'current-segment-indicator' : 'segment-indicator')}></div>);    
     }
     return indicatorGroup;
   };
 
   return (
-    <div className="row">
+    <div className="row pnr-result-header">
       <div className="col">
-        <h5>Ticket {props.itineraryNumber} | {currencySymbol(props.currency)}{Math.round(props.price)}</h5>
+        <h2>
+          <span className="text-bold">Ticket {props.itineraryNumber}</span> | {currencySymbol(props.currency)}{Math.round(props.price)}
+        </h2>
         <p>{itineraryTypeMap(props.segmentType)}</p>
-        <span>{props.itineraryNumber}/{props.segmentCount}</span>
-        <div className="row">
+        <div className="row indicator-row">
+          {segmentIndicators()}
         </div>
       </div>
     </div>

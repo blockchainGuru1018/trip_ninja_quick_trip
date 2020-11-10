7d6687e9bfe3850251647064b84ff16131a4ac7d
Rob Dumont <rob.dumont99@gmail.com>
2020-11-10 09:35:53 -0500

    Update MarkupHelper.ts

--- src/helpers/MarkupHelper.ts
+++ src/helpers/MarkupHelper.ts
@@ -3,9 +3,12 @@ import { numberOfItineraries } from './MiscHelpers';
 
 export const calculateDistributedMarkup = (tripMarkup: number, segments: Array<Segment>) => {
   const itineraryCount: number = numberOfItineraries(segments);
+  const segmentPrice: number = 50;
+  const tripPrice: number = 100;
   if (tripMarkup > 0) {
-    return tripMarkup/itineraryCount;
+    return tripMarkup * segmentPrice/tripPrice;
   }
+
   return 0;
 };
 

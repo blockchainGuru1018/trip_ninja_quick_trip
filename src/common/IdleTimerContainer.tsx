91fa6bf9c339e863bea1e0a2f15e6cf9e6bab0cb
Rob Dumont <rob.dumont99@gmail.com>
2020-11-19 09:19:41 -0500

    idle timer test

--- src/common/IdleTimerContainer.tsx
+++ src/common/IdleTimerContainer.tsx
@@ -2,21 +2,42 @@ import React, { useRef } from 'react';
 import IdleTimer from 'react-idle-timer';
 import { logout } from '../actions/AuthActions';
 
-interface IdleTimerContainterProps {
+interface IdleTimerContainerProps {
   logout: typeof logout
 }
 
-function IdleTimerContainer (props: IdleTimerContainterProps) {
-  const idleTimerRef = useRef(null);
-  return (
-    <div>
-      <IdleTimer
-        ref={idleTimerRef}
-        timeout={14400000}
-        onIdle={() => props.logout()}
-      />
-    </div>
-  );
+class IdleTimeContainer extends React.Component<IdleTimerContainerProps> {
+
+  render() {
+    console.log("Loaded");
+    let idleTimer = null;
+    return (
+      <div>
+        <IdleTimer
+          ref={ref => { idleTimer = ref; }}
+          timeout={1000 * 60 * 1}
+          onActive={this.handleOnActive}
+          onIdle={this.handleOnIdle}
+          onAction={this.handleOnAction}
+          debounce={250}
+        />
+      </div>
+    );
+  }
+
+  handleOnAction (event: any) {
+    console.log('user did something', event);
+  }
+
+  handleOnActive (event: any) {
+    console.log('user is active', event);
+   
+  }
+
+  handleOnIdle (event: any) {
+    console.log('user is idle', event);
+    this.props.logout();
+  }
 }
 
-export default IdleTimerContainer;
+export default IdleTimeContainer;

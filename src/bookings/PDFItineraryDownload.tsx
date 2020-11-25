9a30f4ea29fb6f2af61599ab1669986bb0d6fcc7
Rob Dumont <rob.dumont99@gmail.com>
2020-11-25 11:16:19 -0400

    add passengers to pdf

--- src/bookings/PDFItineraryDownload.tsx
+++ src/bookings/PDFItineraryDownload.tsx
@@ -8,6 +8,7 @@ import { AuthDetails } from "../auth/AuthInterfaces";
 import { FlightResultsDetails, Results, defaultResults, ResultsDetails, Segment } from "../trip/results/ResultsInterfaces";
 import FlightResultsPath from "../common/FlightResultsPath";
 import { Booking, BookingItinerary, BookingSegment } from "./BookingsInterfaces";
+import { BookingDetails, PassengerInfo } from "../trip/book/BookInterfaces";
 import { withTranslation, WithTranslation } from "react-i18next";
 import FareRulesPreview from "../common/FareRulesPreview";
 import { prepareSvgForPdf } from "../helpers/PdfHelpers";
@@ -27,6 +28,7 @@ interface  PDFItineraryDownloadProps extends WithTranslation {
   params?: any;
   resultsDetails: ResultsDetails;
   pricingDetails: PricingDetails;
+  bookingDetails: BookingDetails;
   setErrorDetails: typeof setErrorDetails;
 }
 
@@ -112,7 +114,20 @@ export class PDFItineraryDownload extends React.Component<PDFItineraryDownloadPr
             flightDetailsDisplay={true}
           />
         }
+        {this.passengerInfoHtml()}
         {this.getPageNumHTML(numPages, numPages)}
+      </div>
+    );
+  }
+
+  passengerInfoHtml = () => {
+    let passengers: Array<PassengerInfo> = this.props.booking?.details?.passengers ? this.props.booking?.details?.passengers : this.props.bookingDetails.passengers;
+    return (
+      <div>
+        <h5 className="passenger-section-header">Passengers</h5>
+        { passengers.map((passenger: PassengerInfo) => {
+          return <p className="passenger-name">{passenger.first_name} {passenger.last_name}</p>;
+        })}
       </div>
     );
   }

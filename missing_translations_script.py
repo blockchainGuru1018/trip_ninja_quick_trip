4b14a3c4b001db97d1fbeaafb71ec791724706d2
k.mcclymont <kieran.mcclymont@tripninja.io>
2020-12-02 14:29:08 -0400

    - fixed the url on the translations

--- missing_translations_script.py
+++ missing_translations_script.py
@@ -1,40 +1,31 @@
-#!/usr/bin/env python
+# !/usr/bin/env python
 # coding: utf-8
 
-# In[4]:
 
 
 import json
-
-
-# In[5]:
-
-
-with open('/Users/kieranmcclymont/Documents/Projects/trip_ninja_quick_trip/src/assets/translations/en_uk/common.json') as json_file:
-    uk_data = json.load(json_file)
-with open('/Users/kieranmcclymont/Documents/Projects/trip_ninja_quick_trip/src/assets/translations/es/common.json') as json_file:
-    es_data = json.load(json_file)
-with open('/Users/kieranmcclymont/Documents/Projects/trip_ninja_quick_trip/src/assets/translations/fr/common.json') as json_file:
-    fr_data = json.load(json_file)
-with open('/Users/kieranmcclymont/Documents/Projects/trip_ninja_quick_trip/src/assets/translations/jp/common.json') as json_file:
-    jp_data = json.load(json_file)
-
-
-# In[6]:
-
+import os
+import pprint
+import operator
 
 from functools import reduce
-import operator
+
+pp = pprint.PrettyPrinter(indent=2)
+
+with open(f'{os.getcwd()}/src/assets/translations/en_uk/common.json') as json_file:
+    uk_data = json.load(json_file)
+with open(f'{os.getcwd()}/src/assets/translations/es/common.json') as json_file:
+    es_data = json.load(json_file)
+with open(f'{os.getcwd()}/src/assets/translations/fr/common.json') as json_file:
+    fr_data = json.load(json_file)
+with open(f'{os.getcwd()}/src/assets/translations/jp/common.json') as json_file:
+    jp_data = json.load(json_file)
 
 def get_from_dict(dataDict, mapList):
     try:
         return reduce(operator.getitem, mapList, dataDict)
     except KeyError:
         return None
-
-
-# In[7]:
-
 
 def compare_all_translations(key_list):
     missing_list = []
@@ -43,11 +34,6 @@ def compare_all_translations(key_list):
     if not get_from_dict(fr_data, key_list) or get_from_dict(fr_data, key_list) == "": missing_list.append('fr')
     if not get_from_dict(jp_data, key_list) or get_from_dict(jp_data, key_list) == "": missing_list.append('jp')
     return missing_list
-
-
-# In[8]:
-
-
 
 key_list = []
 complete = False
@@ -83,13 +69,4 @@ while not complete:
     if list_number[0] == len(list(uk_data.keys())):
         complete = True
 
-print(missing_dict)
-
-                
-
-
-# In[ ]:
-
-
-
-
+pp.pprint(missing_dict)
